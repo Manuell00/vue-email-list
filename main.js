6e49fee33edb91d2dfa5ab8add151829af63f9c7
Manuell00 <manuell.caselli@gmail.com>
2023-05-29 17:56:21 +0200

    Complletamento hover

--- main.js
+++ main.js
@@ -1,12 +1,29 @@
 const {createApp} = Vue
 createApp({
-    // Definisco i data
+
+    // Definisco i DATI
     data() {
         return {
+            // Creo una lsta vuota che conterrà le email dell'API
             emailList: [],
+
+            // Creo una variabile Booleana
             allEmailsGenerated: false
         }
     },
+
+    // Definisco i METODI
+    methods: {
+        // Creo una funzione per cambiare tonalità di colori alla pagina
+        toggleMode(){
+            const body = document.querySelector('#myapp');
+            const title = document.querySelector('#title');
+            body.classList.toggle('dark-mode');
+            title.classList.toggle('dark-mode');
+        }
+        },
+
+
     mounted() {
         for (let i = 0; i < 10; i++) {
         axios
